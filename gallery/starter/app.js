ba55270ebc08ed71504b3fd7cca86efca2b290ab
Raj <hmacha.jscript@gmail.com>
2020-10-10 16:55:44 +0530

    gallery html is done

--- gallery/starter/app.js
+++ gallery/starter/app.js
@@ -7,3 +7,18 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+function Gallery(element){
+  this.list  = [...element.querySelectorAll('img')];
+  this.modal = getElement('.modal');
+  this.closeBtn = this.modal.querySelector('.close-btn');
+  this.prevBtn = this.modal.querySelector('.prev-btn');
+  this.nextBtn = this.modal.querySelector('.next-btn');
+  this.mainImg = this.modal.querySelector('.main-img');
+  this.modalImgs = this.modal.querySelector('.modal-images');
+  this.modalImg = this.modal.querySelectorAll('.modal-img');
+  console.log([...this.modalImg]);
+}
+
+const nature = new Gallery(getElement('.nature'))
+const city = new Gallery(getElement('.city'))
